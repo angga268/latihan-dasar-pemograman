39e5c972d1a7dffa6ee220a563861d85674490ab
angga268 <91416171+angga268@users.noreply.github.com>
2022-09-29 11:11:19 +0700

    Add files via upload

---
+++ aplikasiBiodata.py
@@ -0,0 +1,13 @@
+# Aplikasi Biodata Created by Angga Agustian
+
+# memasukan nama
+print ("Yuk lengkapi data diri..")
+#  MASUKAN INPUT 
+nama = input("masukan nama anda")
+kelas = input("masukan kelas anda")
+prodi = input("masukan prodi anda")
+# print / output
+
+print ("nama anda adalah : " , nama )
+print ("kelas anda adalah : " , kelas )
+print ("prodi anda adalah : " , prodi )
