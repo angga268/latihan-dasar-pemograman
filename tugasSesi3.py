a737283f804ab1d24b45996a20a42c7423cf959a
angga268 <91416171+angga268@users.noreply.github.com>
2022-10-06 09:22:34 +0700

    Add files via upload

---
+++ tugasSesi3.py
@@ -0,0 +1,10 @@
+# Tugas Sesi 3 by Angga Agustian
+nama = "Angga Agustian"
+kelas = "TI22A"
+jurusan = "Teknik Informatika"
+quote = "TETAP BERNAFAS WALAU TAK MELAKUKAN APAPUN!!!"
+
+print("hi saya ", nama)
+print("saya dari kelas ", kelas)
+print(jurusan)
+print("Motivasi hidup saya", quote)
